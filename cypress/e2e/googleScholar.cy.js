f5fb0be4a06d89f98e9a806acca4e7a7db7660cc
Julio98oliveira <julio.csoliveira98@gmail.com>
2026-05-06 20:43:43 -0300

    Ignora erro externo do Bing

--- cypress/e2e/googleScholar.cy.js
+++ cypress/e2e/googleScholar.cy.js
@@ -1,22 +1,21 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+
 describe('Pesquisa Bing', () => {
 
   it('Pesquisar tema e validar resultados', () => {
 
-    // Acessa o Bing
     cy.visit('https://www.bing.com')
 
-    // Pesquisa o tema
     cy.get('textarea[name="q"], input[name="q"]')
       .type('uso da inteligência artificial na medicina{enter}')
 
-    // Valida se os resultados apareceram
     cy.get('#b_results')
       .should('be.visible')
 
-    // Aguarda carregamento
     cy.wait(2000)
 
-    // Screenshot personalizada
     cy.screenshot('resultado-pesquisa-bing')
 
   })
